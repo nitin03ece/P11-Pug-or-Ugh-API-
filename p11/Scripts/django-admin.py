db94574ad48a7646fbb1f10cf605431ed228a7b2
Nitin Sinha <nitin03ece@outlook.com>
2019-04-19 09:56:18 +0530

    Initial Update

---
+++ p11/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\nsinha\videos\py\project\pr35cc~1\3eccf~1.git\p11-pu~1\p11\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
